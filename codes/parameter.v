1d78d9ded27602ccf5c01e219b415aa9c68b367f
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-04-07 15:58:18 +0530

    Create parameter.v

---
+++ codes/parameter.v
@@ -0,0 +1,10 @@
+/***************************************/
+ /****************** Definition file ********/ 
+/************** **********************************************/ 
+`define INPUTFILENAME "your_image.hex" // Input file name 
+`define OUTPUTFILENAME "output.bmp" // Output file name 
+// Choose the operation of code by delete 
+// in the beginning of the selected line 
+//`define BRIGHTNESS_OPERATION 
+//`define INVERT_OPERATION 
+//`define THRESHOLD_OPERATION
